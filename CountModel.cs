46c087bdcb8b2e33c09d3b0e15a4018c7d2cfec7
havok <havok@DESKTOP-AHMKGCB>
2022-08-04 19:02:21 -0300

    Add project files.

---
+++ CountModel.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Cronometer_WPF
+{
+    public class CountModel : INotifyPropertyChanged, ICommand
+    {
+
+        DispatcherTimer dt = new DispatcherTimer();
+
+        public CountModel()
+        {
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += IncreaseTimer;
+        }
+
+        private void IncreaseTimer(object sender, EventArgs e)
+        {
+            if (dt.IsEnabled)
+                Seconds++;
+
+            if (Seconds > 60)
+            {
+                Minutes++;
+                Seconds = 0;
+            }
+            if (Minutes > 60)
+            {
+                Hours++;
+                Minutes = 0;
+            }
+        }
+
+        #region Properties
+
+        private int _seconds;
+        public int Seconds
+        {
+            get => _seconds;
+            set
+            {
+                _seconds = value;
+                OnPropertyChanged(nameof(Seconds));
+            }
+        }
+        private int _minutes;
+        public int Minutes
+        {
+            get => _minutes;
+            set
+            {
+                _minutes = value;
+                OnPropertyChanged(nameof(Minutes));
+            }
+        }
+        private int _hours;
+        public int Hours
+        {
+            get => _hours;
+            set
+            {
+                _hours = value;
+                OnPropertyChanged(nameof(Hours));
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand _start;
+        public ICommand Start
+        {
+            get 
+            { 
+                if(_start == null)
+                    _start = new RelayCommand(ExecuteStartTimer, CanExecute);
+
+                return _start;
+            }               
+        }
+
+        private ICommand _stop;
+        public ICommand Stop
+        {
+            get
+            {
+                if (_stop == null)
+                    _stop = new RelayCommand(ExecuteStopTimer, CanExecute);
+
+                return _stop;
+            }
+        }
+
+        private ICommand _pause;
+        public ICommand Pause
+        {
+            get
+            {
+                if (_pause == null)
+                    _pause = new RelayCommand(ExecutePauseTimer, CanExecute);
+
+                return _pause;
+            }
+            
+        }
+
+        private void ExecutePauseTimer() => dt.Stop();
+
+        private void ExecuteStartTimer() => dt.Start();
+
+        private void ExecuteStopTimer()
+        {
+            dt.Stop();
+            Seconds = 0;
+            Minutes = 0;
+            Hours = 0;
+        }
+
+        #endregion
+
+        private bool CanExecute() => true;       
+
+        #region INPC & ICommand
+
+        public event PropertyChangedEventHandler PropertyChanged; 
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+        }
+
+        #endregion
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Cronometer_WPF
+{
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) => DragMove();
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) =>
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+
+    }
+}
